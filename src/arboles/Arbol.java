9bde7f940e9e56ac48949356729d114869962f7d
plaiaundi <plaiaundi@HZ309811.H012108.NET>
2023-01-19 11:42:53 +0100

    sera

---
+++ src/arboles/Arbol.java
@@ -0,0 +1,52 @@
+package arboles;
+
+public class Arbol {
+private int id;
+private String nombreComun;
+private String nombreCientifuico;
+private String habitat;
+private int altura;
+private  String origen;
+
+public void Arbol() {
+	
+}
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNombreComun() {
+	return nombreComun;
+}
+public void setNombreComun(String nombreComun) {
+	this.nombreComun = nombreComun;
+}
+public String getNombreCientifuico() {
+	return nombreCientifuico;
+}
+public void setNombreCientifuico(String nombreCientifuico) {
+	this.nombreCientifuico = nombreCientifuico;
+}
+public String getHabitat() {
+	return habitat;
+}
+public void setHabitat(String habitat) {
+	this.habitat = habitat;
+}
+public int getAltura() {
+	return altura;
+}
+public void setAltura(int altura) {
+	this.altura = altura;
+}
+public String getOrigen() {
+	return origen;
+}
+public void setOrigen(String origen) {
+	this.origen = origen;
+}
+
+}
---
+++ src/arboles/GestorArboles.java
@@ -0,0 +1,24 @@
+package arboles;
+
+import java.util.Scanner;
+
+public class GestorArboles {
+ public static void run() {
+	 Scanner scan = new Scanner(System.in);
+	 System.out.println("1 insertar árbol\r\n"
+	 		+ "2 eliminar árbol\r\n"
+	 		+ "3 modificar información del árbol\r\n"
+	 		+ "4 visualizar árboles\r\n"
+	 		+ "5 salir");
+	 int opciones=-1;
+	 opciones=Integer.()scan.nextLine();
+	 switch (key) {
+	case value: {
+		
+		yield type;
+	}
+	default:
+		throw new IllegalArgumentException("Unexpected value: " + key);
+	}
+ }
+}
