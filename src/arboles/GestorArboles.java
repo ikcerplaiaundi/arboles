706ba0225a98a0c3f41b738b941cebf95a57577c
plaiaundi <plaiaundi@HZ309811.H012108.NET>
2023-01-23 13:25:36 +0100

    PreparedStatement como nuevo metodo

--- src/arboles/GestorArboles.java
+++ src/arboles/GestorArboles.java
@@ -2,6 +2,7 @@ package arboles;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,9 +14,16 @@ public class GestorArboles {
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "";
 	public static void run() throws ClassNotFoundException, SQLException {
+		//new security method
+		//PreparedStatement preparedSt = con.prepareStatement("INSERT INTO animales (nombre) VALUES (?,?)");
+		//preparedSt.setString(1,parte[0]);
+		//preparedSt.setString(2,"salado");
+		//preparedSt.setBoolean(3,true);
+		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		String url="jdbc:mysql://" + HOST + "/" + BBDD;
 		Connection con = DriverManager.getConnection(url, USERNAME, PASSWORD);
+		PreparedStatement preparedSt ;
 		Statement st = con.createStatement();
 		Scanner scan = new Scanner(System.in);
 		int opciones;
@@ -34,7 +42,14 @@ public class GestorArboles {
 				try {
 				System.out.println("insertar árbol int id, String nombreComun,String nombreCientifuico,String habitat,int altura,String origen");
 				partes=scan.nextLine().split(",");
-				st.execute("INSERT INTO `arboles`( `nombre_comun`, `nombre_cientifico`, `habitat`, `altura`, `origen`) VALUES ('"+partes[1]+"','"+partes[2]+"','"+partes[3]+"','"+partes[4]+"','"+partes[5]+"')");
+				//st.execute("INSERT INTO `arboles`( `nombre_comun`, `nombre_cientifico`, `habitat`, `altura`, `origen`) VALUES ('"+partes[1]+"','"+partes[2]+"','"+partes[3]+"','"+partes[4]+"','"+partes[5]+"')");
+				preparedSt = con.prepareStatement("INSERT INTO animales (nombre) VALUES (?,?,?,?,?)");
+				preparedSt.setString(1,partes[0]);
+				preparedSt.setString(2,partes[1]);
+				preparedSt.setString(3,partes[2]);
+				preparedSt.setInt(4, Integer.parseInt(partes[3]));
+				preparedSt.setString(5,partes[4]);
+				
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
@@ -45,6 +60,7 @@ public class GestorArboles {
 				partes=scan.nextLine().split(",");
 				String sentDELETE="DELETE FROM arboles WHERE id ='"+partes[0]+"'";
 				st.executeUpdate(sentDELETE);
+				
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
@@ -53,8 +69,17 @@ public class GestorArboles {
 				try {
 				System.out.println("insertar árbol int id, String nombreComun,String nombreCientifuico,String habitat,int altura,String origen");
 				partes=scan.nextLine().split(",");
-				String sentUpdate="UPDATE arboles SET `nombre_comun`='"+partes[1]+"',`nombre_cientifico`='"+partes[2]+"',`habitat`='"+partes[3]+"',`altura`='"+partes[4]+"',`origen`='"+partes[5]+"' WHERE id='"+partes[0]+"'";
-				st.executeUpdate(sentUpdate);
+				//String sentUpdate="UPDATE arboles SET `nombre_comun`='"+partes[1]+"',`nombre_cientifico`='"+partes[2]+"',`habitat`='"+partes[3]+"',`altura`='"+partes[4]+"',`origen`='"+partes[5]+"' WHERE id='"+partes[0]+"'";
+				//st.executeUpdate(sentUpdate);
+				
+				preparedSt = con.prepareStatement("INSERT INTO animales (nombre) VALUES (?,?,?,?,?)");
+				
+				preparedSt.executeUpdate(partes[0],1);
+				preparedSt.executeUpdate(partes[1],2);
+				preparedSt.executeUpdate(partes[2],3);
+				preparedSt.executeUpdate(partes[3],4);
+				preparedSt.executeUpdate(partes[4],5);
+				
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
